4bb91bc5d138ef0384f26425f5ba5933ef68d7a2
r3lic <87726096+r3lic-pnw@users.noreply.github.com>
2024-09-22 02:28:00 -0700

    Update search.js

--- search.js
+++ search.js
@@ -2,6 +2,7 @@ const {readdir} = require("fs").promises;
 
 function search(folder) {
   return readdir(folder, { withFileTypes: true }).then(contents => {
+    if (contents.length === 0) return[];
     return Promise.all(contents.map(item => {
       if (item.isDirectory()) {
         return search(item.path).then(subfileList => ({ path: item.path, subfiles: subfileList }));
